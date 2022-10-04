70a72f220e76ae58c61cd886a17691e0c2690f44
Nicholas <blueretrograde@gmail.com>
2022-10-04 17:22:48 +0100

    As far as I got...

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
 import { pokemonArray } from './data/pokemon.js'
 
-console.log(pokemonArray)
+//SELECTORS
+
+const cardContainer = document.querySelector(".card-container")
+
+
+//FUNCTIONS
+
+
+const getPokemonValue = (pokemon) => {
+
+    //let MyPokemonArray = MyPokemonArray.toString();
+
+}
+
+
+
+
+let pokemonName; 
+let pokemonImage;
+let pokemonText;
+
+const getPokemonCard = (pokemonName, pokemonImage, pokemonText) => {
+        cardContainer.innerHTML = `
+<h3 class="card__heading">${pokemonName}</h3>
+<img class="card__image">${pokemonImage}</img>
+<p class="card__text">${pokemonText}</p>
+`
+}
+
+
+getPokemonCard("Test", "testing", "test,test")
+
+
+pokemonName =
+    pokemonArray.forEach(element => {
+
+        pokemonArray[1]
+        console.log(pokemonName, "pokemonName")
+    });
+
+    
+
+
+
+    //create a header for name
+
+
+
+    //create an img image
+
+    
+
+
+
+    //create a p for type
